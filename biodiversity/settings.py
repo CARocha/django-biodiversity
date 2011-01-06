a666fc8f4bc8b43b35d10d472fb0795a12aa0c20
sacrac <carcas@sacrac.info>
2011-01-06 17:18:08 -0600

    bug fix

--- biodiversity/settings.py
+++ biodiversity/settings.py
@@ -96,7 +96,7 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
-    'diversity',
+    'biodiversity.diversity',
 )
 
 # A sample logging configuration. The only tangible logging
