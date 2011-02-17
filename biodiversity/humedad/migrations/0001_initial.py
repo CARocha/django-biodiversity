6ef1760ab7e0cada870821e3d61998f3b08bc46a
Adolfo Fitoria <adolfo.fitoria@gmail.com>
2011-02-17 14:21:04 -0600

    reparando migración erronea

--- biodiversity/humedad/migrations/0001_initial.py
+++ biodiversity/humedad/migrations/0001_initial.py
@@ -7,15 +7,46 @@ from django.db import models
 class Migration(SchemaMigration):
 
     def forwards(self, orm):
-        pass
+        
+        # Adding model 'Humedad'
+        db.create_table('humedad_humedad', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('mes', self.gf('django.db.models.fields.IntegerField')()),
+            ('ano', self.gf('django.db.models.fields.IntegerField')()),
+            ('humedad', self.gf('django.db.models.fields.FloatField')()),
+            ('zona', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['diversity.Lugar'])),
+        ))
+        db.send_create_signal('humedad', ['Humedad'])
 
 
     def backwards(self, orm):
-        pass
+        
+        # Deleting model 'Humedad'
+        db.delete_table('humedad_humedad')
 
 
     models = {
-        
+        'diversity.lugar': {
+            'Meta': {'object_name': 'Lugar'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'latitud': ('django.db.models.fields.DecimalField', [], {'null': 'True', 'max_digits': '8', 'decimal_places': '5', 'blank': 'True'}),
+            'longitud': ('django.db.models.fields.DecimalField', [], {'null': 'True', 'max_digits': '8', 'decimal_places': '5', 'blank': 'True'}),
+            'nombre': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
+            'pais': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['diversity.Pais']"})
+        },
+        'diversity.pais': {
+            'Meta': {'object_name': 'Pais'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'nombre': ('django.db.models.fields.CharField', [], {'max_length': '200'})
+        },
+        'humedad.humedad': {
+            'Meta': {'object_name': 'Humedad'},
+            'ano': ('django.db.models.fields.IntegerField', [], {}),
+            'humedad': ('django.db.models.fields.FloatField', [], {}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'mes': ('django.db.models.fields.IntegerField', [], {}),
+            'zona': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['diversity.Lugar']"})
+        }
     }
 
     complete_apps = ['humedad']
