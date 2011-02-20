0b49405e9de75e38139a7b4d049be7e1d52a6826
Byron Corrales <bcorrales@bcorrales-laptop.(none)>
2011-02-19 23:10:52 -0600

    Terminando templates de eventos

--- biodiversity/settings.py
+++ biodiversity/settings.py
@@ -23,7 +23,7 @@ USE_I18N = True
 
 # If you set this to False, Django will not format dates, numbers and
 # calendars according to the current locale
-USE_L10N = False 
+USE_L10N = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
