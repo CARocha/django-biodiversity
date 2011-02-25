1223829f9a9c31574122a77f477a03a319a78c36
Helmy Giacoman <helmygb@gmail.com>
2011-02-25 16:40:18 -0600

    index de askbot

--- biodiversity/askbotmini/views.py
+++ biodiversity/askbotmini/views.py
@@ -4,8 +4,10 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 from forms import *
 from models import *
+import datetime
 
 def index(request):
+    questions = Question.objects.all().order_by('-last_answer_date')
     return render_to_response('askbotmini/index.html', locals(), context_instance=RequestContext(request))
 
 @login_required
@@ -18,6 +20,7 @@ def ask_question(request):
         obj.description = request.POST['description']
         obj.user = request.user
         obj.views = 0
+        obj.date_created = datetime.datetime.now()
         obj.tags = request.POST['tags']
         obj.save()
         return HttpResponseRedirect('/askbot/')
