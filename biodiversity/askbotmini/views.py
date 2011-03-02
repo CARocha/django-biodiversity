a0ff50e5f4243e33a9f31f7c4babf2a1d0f37a8c
Helmy Giacoman <helmygb@gmail.com>
2011-03-02 12:04:24 -0600

    notify function

--- biodiversity/askbotmini/views.py
+++ biodiversity/askbotmini/views.py
@@ -2,6 +2,9 @@ from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
+from django.template.loader import render_to_string
+from django.core.mail import send_mail
+from django.contrib.auth.models import User
 from tagging.models import Tag, TaggedItem
 from forms import *
 from models import *
@@ -44,16 +47,18 @@ def index(request):
 def ask_question(request):
     if request.method == 'POST':
         form = AskForm(request.POST)
-#        if form.is_valid():
-        obj = Question()
-        obj.question = request.POST['question']
-        obj.description = request.POST['description']
-        obj.user = request.user
-        obj.views = 0
-        obj.date_created = datetime.datetime.now()
-        obj.tags = request.POST['tags']
-        obj.save()
-        return HttpResponseRedirect('/foro/?tab=latest')
+        if form.is_valid():
+            obj = Question()
+            obj.question = request.POST['question']
+            obj.description = request.POST['description']
+            obj.user = request.user
+            obj.views = 0
+            obj.date_created = datetime.datetime.now()
+            obj.tags = request.POST['tags']
+            obj.save()
+            if form.cleaned_data['notify']:
+                notify_all(obj)
+            return HttpResponseRedirect('/foro/?tab=latest')
     else:
         form = AskForm()
     return render_to_response('askbotmini/ask_question.html', RequestContext(request, locals()))
@@ -99,3 +104,16 @@ def tagged_in(request, tag_name):
     tag = get_object_or_404(Tag, name=tag_name)
     questions = TaggedItem.objects.get_by_model(Question, tag.name).order_by('-last_answer_date')
     return render_to_response('askbotmini/tagged_in.html', RequestContext(request, locals()))
+
+
+#este sera el hilo para mandar mails
+def notify_all(question):
+    site = 'http://localhost:8000/foro/questions/'
+    users = User.objects.all().exclude(username='admin')
+    contenido = render_to_string('askbotmini/notify_new_question.txt', {'question': question,
+                        'url': '%s%s' % (site, question.id),
+                        'url_answer': '%s%s/#answer' % (site, question.id),
+                        })
+    for user in users:
+        if user.email:
+            send_mail('Nueva pregunta', contenido, 'develop@simasni.org', [user.email, ])
