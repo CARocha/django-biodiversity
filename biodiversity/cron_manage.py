84483b7370c234d4dc5703c5cece90912268bf17
Helmy Giacoman <helmygb@gmail.com>
2011-03-09 14:08:29 -0600

    adding cron jo

--- biodiversity/cron_manage.py
+++ biodiversity/cron_manage.py
@@ -1,10 +1,13 @@
-#!/usr/bin/env python
+#!/home/simasnio/bin/python
 from django.core.management import execute_manager
+import sys
+
+sys.path.insert(0, "/home/simasnio/projects/")
 try:
     import settings # Assumed to be in the same directory.
 except ImportError:
     import sys
-    sys.stderr.write("Error: Can't find the file 'settings.py' in the directory containing %r. It appears you've customized things.\nYou'll have to run django-admin.py, passing it your settings module.\n(If the file settings.py does indeed exist, it's causing an ImportError somehow.)\n" % __file__)
+    sys.stderr.write("Error: Can't find the file 'settings.py' in the directory containing %r. It appears you've customized things.\n$
     sys.exit(1)
 
 if __name__ == "__main__":
